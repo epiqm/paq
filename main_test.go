20d19fa02f5d1630475a563bdf95c686a2fdd6fd
epiqm <epiqmax@gmail.com>
2018-04-05 06:48:45 +0300

    testing complete

--- main_test.go
+++ main_test.go
@@ -1,7 +1,13 @@
+// Copyright 2018 Maxim R. <epiqm.github.io>
+
+// Packs files into a package then unpacks.
+// Compares extracted files with original ones.
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -60,15 +66,46 @@ func TestScan(t *testing.T) {
 }
 
 func TestUnpack(t *testing.T) {
+	// get working directory
+	wd, _ := os.Getwd()
+	for !strings.HasSuffix(wd, "paq") {
+		wd = filepath.Dir(wd)
+	}
+
 	var packageNames []string
 
-	// append created package
+	// append package that was created
 	packageNames = append(packageNames, "test.pq")
 
 	// test unpack function
 	result := Unpack(packageNames, "./")
 	if result != true {
 		t.Error("Failed to extract package.")
+	}
+
+	var filesPath []string // original files hashmap
+	filesPath = append(filesPath, fmt.Sprintf("%s/tests/note.txt", wd))
+	filesPath = append(filesPath, fmt.Sprintf("%s/tests/rabbit.png", wd))
+
+	var extractPath []string // extracted files hashmap
+	extractPath = append(filesPath, fmt.Sprintf("%s/note.txt", wd))
+	extractPath = append(filesPath, fmt.Sprintf("%s/rabbit.png", wd))
+
+	// compare extracted files to original ones
+	for k, v := range filesPath {
+		f1, err := ioutil.ReadFile(v)
+		if err != nil {
+			t.Error("Failed to read original file.")
+		}
+
+		f2, err := ioutil.ReadFile(extractPath[k])
+		if err != nil {
+			t.Error("Failed to read extracted file.")
+		}
+
+		if bytes.Equal(f1, f2) == false {
+			t.Error("Extracted content does not match original file.")
+		}
 	}
 
 	// remove temporary files
